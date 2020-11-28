547de4e8443253298874e86f4c2b32c41169582f
Divya Garg <divyagarg2601@gmail.com>
2020-11-28 12:22:40 +0530

    removed unnecessary files

--- internalFunc.py
+++
@@ -1,8 +0,0 @@
-from urllib import request
-import re
-res = request.urlopen('https://www.youtube.com/results?search_query=naach-meri-rani')
-pattern = re.compile(r'\"videoId\":\"(.){11}\"')
-search_results = pattern.finditer(str(res.read()))
-for result in search_results:
-    string = result.group()[11:-1]
-    print("https://www.youtube.com/watch?v={}".format(string))
